ea2442627d7454089a0e2c8e6f31f06879bb6435
Igor <33993@ma-web.nl>
2022-05-12 20:32:51 +0200

    leuk 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const h1 = document.getElementById("js--title")
+const info = document.getElementById("js--info")
 
 console.log(h1)
 
-setInterval(function () { h1.style.animation = "fadeOut 0.5s ease forwards"},5000 )
-setInterval(function () { h1.style.display = "none"},5500)
+setInterval(function () { h1.style.animation = "fadeOut 0.5s ease forwards" },3600 )
+setInterval(function () { h1.style.display = "none",info.style.display="flex"},4000)
