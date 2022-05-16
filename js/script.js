20aa2f21e40cc81a04c9842be31a34d337c7641b
Igor <33993@ma-web.nl>
2022-05-16 12:21:53 +0200

    Index af

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const h1 = document.getElementById("js--title")
 const info = document.getElementById("js--info")
+const login = document.getElementById("js--login")
 
 console.log(h1)
 
 setInterval(function () { h1.style.animation = "fadeOut 0.5s ease forwards" },2500 )
-setInterval(function () { h1.style.display = "none",info.style.display="flex"},2900)
+setInterval(function () { h1.style.display = "none", info.style.display = "flex" }, 2900)
+
