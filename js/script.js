d77b136aaae3833ba9d882b31dc8b0c2c826fedb
Igor <33993@ma-web.nl>
2022-05-24 13:11:54 +0200

    Klaar met index

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ const login = document.getElementById("js--login")
 
 console.log(h1)
 
-setInterval(function () { h1.style.animation = "fadeOut 0.5s ease forwards" },2500 )
-setInterval(function () { h1.style.display = "none", info.style.display = "flex" }, 2900)
+window.setTimeout(function(){
+    h1.style.animation = "fadeOut 0.5s ease forwards"
+}, 2500);
+
+window.setTimeout(function(){
+    h1.style.display = "none", info.style.display = "flex" 
+}, 2900);
+
+
 
